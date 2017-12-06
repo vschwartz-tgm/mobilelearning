3f8acd3e0764b184fb3f0e567d44dc6c26a637c8
Christoph <ckern@student.tgm.ac.at>
2017-12-06 09:50:07 +0100

    NICHT HOLEN
    Geht nicht(glaub ich)

--- MobileLearning/app/src/main/java/com/example/neeraj/mobilelearning/Settings.java
+++ MobileLearning/app/src/main/java/com/example/neeraj/mobilelearning/Settings.java
@@ -2,15 +2,27 @@ package com.example.neeraj.mobilelearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 /**
  * Diese Klasse soll für die Einstellungen zustädig sein.
  */
 public class Settings extends AppCompatActivity {
+    Spinner dropdownStart;
+    Spinner dropdownEnd;
+    String[] items;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+/*
+        this.dropdownStart = (Spinner) findViewById(R.id.spinnerStart);
+        this.dropdownEnd = (Spinner) findViewById(R.id.spinnerEnd);
+        for(int i = 0; i < 1000; i++) {
+            this.items = new String[i];
+        }*/
     }
 }
